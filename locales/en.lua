2c8784223b6d2003e42491312d38f45cdb6ea281
Raoul <50175388+HariboInc@users.noreply.github.com>
2022-10-06 22:19:47 +0200

    Create en.lua
    Far from done.

---
+++ locales/en.lua
@@ -0,0 +1,10 @@
+Locales['en'] = {
+    ['employee'] = '[E] Employee Catalog',
+    ['catalog'] = '[E] Browse Catalog',
+    ['classes'] = 'Classes',
+    ['dealerb'] = 'Dealer Purchase',
+    ['sell'] = 'Sell Vehicle',
+    ['selld'] = 'Who wants a new vehicle?',
+    ['customer'] = 'Customer ID',
+    ['finance'] = 'Finance: ',
+}
