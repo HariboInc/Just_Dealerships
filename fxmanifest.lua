4d5f71313886caf80586a3df3a428d3fc612d58d
Raoul <50175388+HariboInc@users.noreply.github.com>
2022-10-06 22:01:09 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -10,10 +10,10 @@ shared_scripts {
 
 server_scripts {
   '@mysql-async/lib/MySQL.lua',
-  'server.lua',
+  'server/main.lua',
 }
 
 client_scripts {
   '@es_extended/locale.lua',
-  'client.lua',
+  'client/main.lua',
 }
