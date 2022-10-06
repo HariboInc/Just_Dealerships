9df0197f2917f8ff52206ef4e592ed8594cd11a1
Raoul <50175388+HariboInc@users.noreply.github.com>
2022-10-06 22:36:15 +0200

    Version Checker added.

--- server/main.lua
+++ server/main.lua
@@ -478,3 +478,40 @@ AddEventHandler('just_dealerships:setVehicleOwnedPlayerId', function (playerId, 
 		TriggerClientEvent('esx:showNotification', playerId, 'You have got a new car with plate ' ..vehicleProps.plate..'!', vehicleProps.plate)
 	end)
 end)
+
+
+
+--[[ Version Checker ]]--
+local version = "1.0.0"
+
+if Config.CheckForUpdates then
+    AddEventHandler("onResourceStart", function(resource)
+        if resource == GetCurrentResourceName() then
+            CheckFrameworkVersion()
+        end
+    end)
+end
+
+function CheckFrameworkVersion()
+    PerformHttpRequest("https://raw.githubusercontent.com/HariboInc/Just_Dealerships/main/version.txt", function(err, text, headers)
+        if string.match(text, version) then
+            print(" ")
+            print("--------- ^4JUST_DEALERSHIPS VERSION^7 ---------")
+            print("Just_Dealerships ^2is up to date^7 and ready to go!")
+            print("Running on Version: ^2" .. version .."^7")
+            print("^4https://github.com/HariboInc/Just_Dealerships^7")
+            print("--------------------------------------------")
+            print(" ")
+        else
+          print(" ")
+          print("--------- ^4JUST_DEALERSHIPS VERSION^7 ---------")
+          print("Just_Dealerships ^1is not up to date!^7 Please update!")
+          print("Curent Version: ^1" .. version .. "^7 Latest Version: ^2" .. text .."^7")
+          print("^4https://github.com/HariboInc/Just_Dealerships^7")
+          print("--------------------------------------------")
+          print(" ")
+        end
+
+    end, "GET", "", {})
+
+end
